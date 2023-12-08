55439ba8b0c379f06f3a9a4e7c2e749bb282a1b6
Rober <roberdz30@gmail.com>
2023-12-08 17:46:05 -0300

    Add 1

--- parte1/src/App.jsx
+++ parte1/src/App.jsx
@@ -7,25 +7,49 @@ const Descripcion = () => {
 };
 
 function App() {
-  const [contador, updateContador] = useState(0);
+  const [counters, setCounters] = useState({
+    left: 0,
+    right: 0,
+    mensaje: "Hola Rober querido venis bien",
+  });
+
+  const [clicks, setClicks] = useState([]);
 
   // setInterval(() =>{
   //   updateContador(contador + 1)
   // },1000)
 
-  const handleClik = (e) => {
-    const typeBotton = e.target.id;
-    typeBotton === "Positivo"
-      ? updateContador(contador + 1)
-      : updateContador(contador - 1);
+  // const handleClik = (e) => {
+  //   const typeBotton = e.target.id;
+  //   typeBotton === "Positivo"
+  //     ? updateContador(contador + 1)
+  //     : updateContador(contador - 1);
+  // };
+
+  // const handleReset = () => {
+  //   updateContador(0);
+  // };
+
+  // const isEven = contador % 2 === 0;
+  // const mensajePar = isEven ? "Número Par" : "Número Impar";
+
+  const handleLeft = () => {
+    setCounters({
+      ...counters,
+      left: counters.left + 1,
+      mensaje: "Ultimo click en la Izquierda",
+    });
+    setClicks((prevClicks) => [...prevClicks, "Left"]);
   };
 
-  const handleReset = () => {
-    updateContador(0);
+  const handleRigth = () => {
+    setCounters({
+      ...counters,
+      right: counters.right + 1,
+      mensaje: "Ultimo click en la derecha",
+    });
+    setClicks((prevClicks) => [...prevClicks, "Right"]);
   };
-
-  const isEven = contador % 2 === 0;
-  const mensajePar = isEven ? "Número Par" : "Número Impar";
 
   return (
     <>
@@ -33,7 +57,7 @@ function App() {
       <Mensaje message="Noches" />
       <Descripcion />
       <h1>A ver ahora</h1>
-      <h2>{contador}</h2>
+      {/* <h2>{contador}</h2>
       <p>{mensajePar}</p>
       <button
         id="Positivo"
@@ -44,7 +68,16 @@ function App() {
         Incrementar
       </button>
       <button onClick={handleClik}>Decrementar</button>
-      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleReset}>Reset</button> */}
+
+      {counters.left}
+      <button onClick={handleLeft}>Izquierda</button>
+      <button onClick={handleRigth}>Derecha</button>
+      {counters.right}
+      <p>Historial de Clicks</p>
+      <p>{counters.mensaje}</p>
+      <p>{clicks.join(" - ")}</p>
+      <p>Clicks Totales: {clicks.length}</p>
     </>
   );
 }
