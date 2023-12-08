44f64ab7a475a61c9a47d94ca5e360b7f510f2d6
Rober <roberdz30@gmail.com>
2023-12-08 13:26:21 -0300

    Add

--- parte1/src/App.jsx
+++ parte1/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Mensaje from "./Mensaje";
 
@@ -6,14 +7,46 @@ const Descripcion = () => {
 };
 
 function App() {
+  const [contador, updateContador] = useState(0);
+
+  // setInterval(() =>{
+  //   updateContador(contador + 1)
+  // },1000)
+
+  const handleClik = (e) => {
+    const typeBotton = e.target.id;
+    typeBotton === "Positivo"
+      ? updateContador(contador + 1)
+      : updateContador(contador - 1);
+  };
+
+  const handleReset = () => {
+    updateContador(0);
+  };
+
+  const isEven = contador % 2 === 0;
+  const mensajePar = isEven ? "Número Par" : "Número Impar";
+
   return (
     <>
-      <Mensaje color="red" message="Buenas" />
+      <Mensaje color="red" message="Prueba" />
       <Mensaje message="Noches" />
       <Descripcion />
+      <h1>A ver ahora</h1>
+      <h2>{contador}</h2>
+      <p>{mensajePar}</p>
+      <button
+        id="Positivo"
+        onClick={(e) => {
+          handleClik(e);
+        }}
+      >
+        Incrementar
+      </button>
+      <button onClick={handleClik}>Decrementar</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 }
-
 
 export default App;
--- parte1/src/main.jsx
+++ parte1/src/main.jsx
@@ -1,28 +1,29 @@
 import ReactDOM from "react-dom";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
+import App from "./App";
+// import Header from "./components/Header";
+// import Content from "./components/Content";
+// import Total from "./components/Total";
 
-const App = () => {
-  const course = [
-    { part: "Fundamentals of React", exercise: 10 },
-    { part: "Using props to pass data", exercise: 7 },
-    { part: "State of a component", exercise: 14 },
-  ];
+// const App = () => {
+//   const course = [
+//     { part: "Fundamentals of React", exercise: 10 },
+//     { part: "Using props to pass data", exercise: 7 },
+//     { part: "State of a component", exercise: 14 },
+//   ];
 
-  const sum = course.reduce((accumulator, currentNumber) => {
-    return accumulator + currentNumber.exercise;
-  }, 0);
-  console.log(sum);
-  return (
-    <div>
-      <Header course="Half Stack application development" />
-      {course.map((el, index) => {
-        return <Content key={index} part={el.part} exercises={el.exercise} />;
-      })}
-      <Total exercises={sum} />
-    </div>
-  );
-};
+//   const sum = course.reduce((accumulator, currentNumber) => {
+//     return accumulator + currentNumber.exercise;
+//   }, 0);
+//   console.log(sum);
+//   return (
+//     <div>
+//       <Header course="Half Stack application development" />
+//       {course.map((el, index) => {
+//         return <Content key={index} part={el.part} exercises={el.exercise} />;
+//       })}
+//       <Total exercises={sum} />
+//     </div>
+//   );
+// };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
