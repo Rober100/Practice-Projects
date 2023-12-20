816949ceb055272f63ee89aef97cf55ede95780b
Rober <roberdz30@gmail.com>
2023-12-20 09:33:43 -0300

    Update

--- parte2/index.js
+++ parte2/index.js
@@ -1,10 +1,63 @@
-const http = require("http")
+const express = require("express")
+const app = express()
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" })
-    response.end("Hello World")
+let Notes = [
+    {
+        albumId: 1,
+        id: 1,
+        title: "accusamus beatae ad facilis cum similique qui sunt",
+        url: "https://via.placeholder.com/600/92c952",
+        thumbnailUrl: "https://via.placeholder.com/150/92c952",
+    },
+    {
+        albumId: 1,
+        id: 2,
+        title: "reprehenderit est deserunt velit ipsam",
+        url: "https://via.placeholder.com/600/771796",
+        thumbnailUrl: "https://via.placeholder.com/150/771796",
+    },
+    {
+        albumId: 1,
+        id: 3,
+        title: "officia porro iure quia iusto qui ipsa ut modi",
+        url: "https://via.placeholder.com/600/24f355",
+        thumbnailUrl: "https://via.placeholder.com/150/24f355",
+    },
+];
+
+//const http = require("http")
+
+// const app = http.createServer((request, response) => {
+//     response.writeHead(200, { "Content-Type": "text/plain" })
+//     response.end("Hello World")
+// })
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World</h1>")
 })
 
+app.get("/notes", (request, response) => {
+    response.json(Notes)
+})
+
+app.get("/notes/:id", (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id);
+    const note = Notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+
+    } else {
+        response.status(404).end()
+    }
+
+})
+
+
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
