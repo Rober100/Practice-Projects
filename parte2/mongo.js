a1421fcee8cf9688ba5485ed5df2d30a9d7b45ee
Rober <roberdz30@gmail.com>
2024-01-29 09:52:40 -0300

    ahora2

--- parte2/mongo.js
+++ parte2/mongo.js
@@ -1,3 +1,13 @@
-const mongoose = requiere ("moongose")
+
+const mongoose = require("mongoose")
 const connectionString = "mongodb+srv://roberdz30:DmR@cluster0.lu92fqb.mongodb.net/notes?retryWrites=true&w=majority"
 
+// Conexión a Moongose 
+mongoose.set('strictQuery', false);
+mongoose.connect(connectionString)
+    .then(
+        () => {
+            console.log("Database Connect")
+        }).catch((error) => {
+            console.log("Error de", error);
+        })
