3c25392e1586b5a7d8aea1e5e5cc8ec060854547
Rober <roberdz30@gmail.com>
2024-02-20 18:57:44 -0300

    app

--- parte2/index.js
+++ parte2/index.js
@@ -4,67 +4,83 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const connectDB = require("./mongo.js")
 const Note = require("./models/Note.js")
-
+const morgan = require("morgan")
 // Conexión a MongoDB
 connectDB();
 
 app.use(express.json())
 app.use(cors())
+app.use(morgan("dev"))
 
 // let Note = [];
 
-app.get("/", (request, response) => {
+app.get("/", (request, response, next) => {
     response.send("<h1>Hola, Funciono correctamente</h1>")
 })
 
-app.get("/notes", (request, response) => {
+app.get("/notes", (request, response, next) => {
     Note.find({}).then(notes => {
         response.json(notes)
         mongoose.connection.close()
+    }).catch(err => {
+        next(err)
     })
 })
 
-app.get("/notes/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const note = Notes.find(note => note.id === id)
+app.get("/notes/:id", (request, response, next) => {
+    const id = request.params.id
 
-    if (note) {
-        response.json(note)
+    Note.findById(id).then(note => {
+        if (note) {
+            response.json(note)
 
-    } else {
-        response.status(404).end()
-    }
+        } else {
+            response.status(404).send("No encontramos la nota").end()
+        }
 
+    }).catch(err => {
+        next(err)
+        // console.log(err);
+        // response.status(400).send("Hubo un problema al buscar la nota por favor verifique si estas correctos los campos").end()
+    })
 })
 
 app.post("/create", (request, response) => {
     const note = request.body
 
-
-
-    const newNote = {
-        albumId: 1,
-        id: note.id,
-        title: note.title
+    if (!note.content) {
+        return response.status(404).json({
+            error: "Se requiere el contenido para crear la nota"
+        })
     }
 
-    Notes = [...Notes, newNote]
-    response.status(201).send("Nota agrega con éxito")
+    const newNote = new Note({
+        content: note.content,
+        date: new Date(),
+        import: note.import || false
+    })
+
+    newNote.save().then(saveNote => {
+        response.json(saveNote)
+    })
+
+
 })
 
-app.put("/delete/:id", (request, response) => {
-    const id = parseInt(request.params.id)
+// app.put("/delete/:id", (request, response) => {
+//     const id = parseInt(request.params.id)
 
-    const newNotes = Notes.filter((note) => note.id !== id)
+//     const newNotes = Notes.filter((note) => note.id !== id)
 
-    Notes = newNotes
-    response.status(201).send("Elemento borrado correctamente")
-})
+//     Notes = newNotes
+//     response.status(201).send("Elemento borrado correctamente")
+// })
 
-app.use((request, response) => {
+app.use((error, request, response, next) => {
+    console.log(error.name)
     response.status(404).json({
         error: "Not Found"
-    })
+    }).end()
 })
 
 
--- parte2/models/Note.js
+++ parte2/models/Note.js
@@ -7,6 +7,14 @@ const noteSchema = new Schema({
     import: Boolean
 })
 
+noteSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
 const Note = model("Note", noteSchema)
 
 module.exports = Note
